4fecc0ea0db29af0fbb64a168df547cdd1b1f7c9
SAGAR SATAPATHY <wishsagarks@gmail.com>
2022-10-05 12:01:12 +0530

    pushed wishtube youtube rapid api v3

---
+++ src/utils/constants.js
@@ -0,0 +1,42 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://raw.githubusercontent.com/wishsagarks/freakingwish-portfolio/main/assets/favicon.ico';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'Wish', icon: <CodeIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'ReactJS', icon: <CodeIcon />, },
+  { name: 'NextJS', icon: <CodeIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://raw.githubusercontent.com/wishsagarks/freakingwish-portfolio/main/assets/favicon.ico';
+export const demoChannelUrl = '/channel/UCFWuoFXS-NMKCZrfv6_CSGw';
+export const demoVideoUrl = 'https://youtu.be/xHQfK_ryaPc';
+export const demoChannelTitle = 'wish';
+export const demoVideoTitle = 'Build and Deploy Wish';
+export const demoProfilePicture = 'https://raw.githubusercontent.com/wishsagarks/freakingwish-portfolio/main/assets/favicon.ico'
---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': 'a596075dbfmsha7ed22d9701d8a3p144609jsnc45f4004f49f',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
